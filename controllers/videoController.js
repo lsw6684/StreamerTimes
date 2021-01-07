762064d09209962c49792f3eb8281ebcb712fae8
lsw6684 <lsw6684@hanmail.net>
2021-01-07 11:14:25 +0900

    wtf

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,4 @@
-//Template variables in Pug test
+//Template variables in Pug
 import { videos } from "../db";
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
